100290aa847dafc76c438cf7566802d94988161b
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-04-27 09:49:32 -0400

    orm.js

---
+++ config/orm.js
@@ -0,0 +1,52 @@
+let connection = require("./connection.js");
+function objToSql(ob) {
+  let arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (let key in ob) {
+    let value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+
+      arr.push(key + "=" + value);
+    }
+  }
+}
+
+let orm = {
+  selectAll: function (tableName, cb) {
+    connection.query("select * from ??", [tableName], function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function (burger_name, devoured, cb) {
+    let queryString = "INSERT INTO burgers";
+
+    queryString += " (burger_name, devoured) ";
+    queryString += "values(?, ?)";
+    connection.query(
+      queryString,
+      [burger_name, devoured],
+      function (err, result) {
+        if (err) throw err;
+        cb(result);
+      }
+    );
+  },
+  updateOne: function (tableName, eaten, condition, cb) {
+    console.log(eaten);
+    let queryString = "UPDATE " + tableName;
+    queryString += " SET devoured = " + eaten;
+    queryString += " WHERE ";
+    queryString += condition;
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
