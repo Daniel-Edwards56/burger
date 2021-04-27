0878afc5d08da11a8536a81e057dc81042cea0f6
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-04-27 10:11:19 -0400

    added burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Model import
+const burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+  burger.select(function (data) {
+    res.render("index", { burgers: data });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  const condition = "id = " + req.params.id;
+  burger.update(req.body.devoured, condition, function (data) {
+    if (data.changedRows == 0) {
+      // If no rows were changed, 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  console.log(req.body);
+  burger.insert(req.body.burger_name, req.body.devoured, function (result) {
+    res.json({ id: result.id });
+  });
+});
+
+module.exports = router;
