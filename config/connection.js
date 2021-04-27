3d12a84b9ab90f833212645f88cca7ab2a452930
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-04-27 09:16:16 -0400

    added conection.js

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Node Dependency
+var mysql = require("mysql");
+const dotenv = require("dotenv");
+var connection;
+// Call dotenv configuration
+dotenv.config();
+
+// For Heroku Deployment vs. Local MySQL Database
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Cashew420?",
+    database: "burgers_db" // Add your database
+  });
+}
+
+// Export the Connection
+module.exports = connection;
